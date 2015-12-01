e6d6313db20deb77ee0021fee3a669d5314bd549
jacopofar <jacopo1.farina@gmail.com>
2015-12-01 12:17:10 +0100

    first version able to run the command as a test

--- index.js
+++ index.js
@@ -4,17 +4,45 @@ var nconf = require('nconf');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var NodeCache = require("node-cache");
+
+//this object will map the images SHA256 sums with their captions
+var sha256Captions = new NodeCache({stdTTL: 60*30, checkperiod: 11});
 
 
+//application configuration, use the CLI args, the env variables and then, if nothing was found, the default values
 nconf.argv().env();
 
 nconf.defaults({
-    'port': 5000
+    port: 5000,
+    modelPath: '/mounted/model/model_id1-501-1448236541.t7_cpu.t7',
+    processFolder: '/mounted/Downloads/',
+    useGPU: '-1'
   });
 
-app.use(express.static('/static'));
+var runNeuralTalk = function(callback){
+var spawn = require('child_process').spawn;
+var ntprocess = spawn('th', ['eval.lua','-model',nconf.get('modelPath'),'-image_folder',nconf.get('processFolder'),'-gpuid',nconf.get('useGPU'),'-dump_path','1'],{cwd:'/neuraltalk2/'});
 
+ntprocess.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
 
+ntprocess.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+ntprocess.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+});
+
+};
+app.use(express.static('static'));
+
+app.get('/test',function(req,res){
+console.log("running the command...");
+runNeuralTalk();
+});
 
 //start the server
 server.listen(nconf.get('port'), function () {
