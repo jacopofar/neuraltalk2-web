dfa975f8fca58aed1ca2cd027acaef056dc65167
jacopofar <jacopo1.farina@gmail.com>
2015-12-01 11:16:29 +0100

    open the server and listen

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 'use strict';
+var fs = require('fs');
+var nconf = require('nconf');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 
+
+nconf.argv().env();
+
+nconf.defaults({
+    'port': 5000
+  });
+
+app.use(express.static('/static'));
+
+
+
+//start the server
+server.listen(nconf.get('port'), function () {
+    var port = server.address().port;
+    console.log(' Application started on port', port);
+  });
