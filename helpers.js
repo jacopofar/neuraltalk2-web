d13ccb6a64af4350ddaf9c5942d74d821094a4cc
jacopofar <jacopo1.farina@gmail.com>
2015-12-04 21:50:55 +0100

    add file copy helper

--- helpers.js
+++ helpers.js
@@ -13,7 +13,7 @@ module.exports.downloadFile = function(url, dest, cb) {
   if(url.indexOf('http:') === 0){
     protocolhandler = http;
   }
-  
+
   if(url.indexOf('https:') === 0){
     protocolhandler = https;
   }
@@ -22,22 +22,47 @@ module.exports.downloadFile = function(url, dest, cb) {
     return;
   }
   var request = protocolhandler.get(url, function(response) {
-var hasher = crypto.createHash('sha256');
-hasher.setEncoding('hex');
-response.pipe(hasher);
-response.pipe(file);
+    var hasher = crypto.createHash('sha256');
+    hasher.setEncoding('hex');
+    response.pipe(hasher);
+    response.pipe(file);
     file.on('finish', function() {
       file.close(function(err,resp){
-       if(err){
-        cb(err);
-        return;
-       }
-hasher.end();
-var thisHash = hasher.read();
+        if(err){
+          cb(err);
+          return;
+        }
+        hasher.end();
+        var thisHash = hasher.read();
 
-var detectedFileType = fileType(readChunk.sync(dest, 0, 262)).ext;
+        var detectedFileType = fileType(readChunk.sync(dest, 0, 262)).ext;
         cb(err,{response:resp,extension:detectedFileType, sha256sum:thisHash}); 
       });
     });
   });
-}  
+};
+
+//shamelessly copied from http://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
+module.exports.cp = function(source, target, cb){
+  var cbCalled = false;
+
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
+}
